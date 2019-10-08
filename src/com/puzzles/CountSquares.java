f750275dba2f909b7b84da0b24e243dc170c1d16
AyushVardhan <ayushvardhan1648@gmail.com>
2019-10-08 14:44:04 +0530

    Ayush | day - 7

---
+++ src/com/puzzles/CountSquares.java
@@ -0,0 +1,47 @@
+/*
+Given a sample space S consisting of all perfect squares starting from 1, 4, 9 and so on. You are given a number N, you have to print the number of integers less than N in the sample space S.
+
+Input :
+The first line contains an integer T, denoting the number of test cases.Then T test cases follow. The first line of each test case contains an integer N, denoting the number.
+
+Output :
+Print the answer of each test case in a new line.
+
+Constraints :
+1<=T<=100
+1<=N<=10^18
+
+Example
+Input :
+2
+9
+3
+
+Output :
+2
+1
+ */
+
+package com.puzzles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CountSquares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+        while (testCase-- > 0) {
+            long N = Long.parseLong(br.readLine());
+            long count = 0;
+            for (int i = 1; i <= Math.floor(Math.sqrt(N)); i++) {
+                if (i * i < N) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
