bab9ff4ea127027db2d090bc453feea96cf333b1
AyushVardhan <ayushvardhan1648@gmail.com>
2019-11-13 00:03:33 +0530

    Ayush | day-22

---
+++ src/com/strings/AnagramOfStrings.java
@@ -0,0 +1,42 @@
+package com.strings;
+
+import java.util.*;
+public class AnagramOfStrings{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String s=sc.next();
+            String s1=sc.next();
+            GfG1 g=new GfG1();
+            System.out.println(g.remAnagrams(s,s1));
+        }
+    }
+}
+
+
+class GfG1
+{
+    public int remAnagrams(String s,String s1)
+    {
+        int a[] = new int[26];
+        int sum = 0;
+
+        for (int i = 0; i< s.length(); i++){
+            a[s.charAt(i)-'a']++;
+        }
+
+        for (int i = 0; i< s1.length(); i++){
+            a[s1.charAt(i)-'a']--;
+        }
+
+        for (int i = 0; i < 26; i++){
+            if (a[i]<0){
+                a[i] *= -1;
+            }
+            sum += a[i];
+        }
+
+        return sum;
+    }
+}
