164b17b115cddaf69225da28caed814c6cf65ad2
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2020-03-04 21:33:17 +0530

    Ayush | day-57 | common ancestor

--- src/com/tree/NearestCommonAncestor.java
+++ src/com/tree/NearestCommonAncestor.java
@@ -3,8 +3,10 @@ package com.tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class NearestCommonAncestor {
     static Node buildTree(String str) {
@@ -70,7 +72,6 @@ public class NearestCommonAncestor {
 
         printInorder(root.left);
         System.out.print(root.data + " ");
-
         printInorder(root.right);
     }
 
@@ -85,43 +86,140 @@ public class NearestCommonAncestor {
             int b = Integer.parseInt(input[1]);
             String s = br.readLine();
             Node root = buildTree(s);
-            NearestCommonAncestor g = new NearestCommonAncestor();
-            Node k = g.lca(root, a, b);
+            Node k = lca(root, a, b);
             System.out.println(k.data);
 
         }
     }
 
-    private Node lca(Node root, int n1, int n2) {
+    static Node lca(Node root, int n1, int n2) {
+
+        // base case
         if (root == null)
             return null;
 
-        int head1 = 0;
-        if (n1 != root.data)
-            head1 = findHead(root, n1, root);
-        else head1 = n1;
+        // If either n1 or n2 matches with root's key, report
+        // the presence by returning root (Note that if a key is
+        // ancestor of other, then the ancestor key becomes LCA
+        if (root.data == n1 || root.data == n2)
+            return root;
 
-        System.out.println(head1);
-        return null;
-    }
+        // Look for keys in left and right subtrees
+        Node l = lca(root.left, n1, n2);
+        Node r = lca(root.right, n1, n2);
 
-    private int findHead(Node root, int n1, Node headOfRoot) {
-        int data = 0;
-        if (root == null)
-            return 0;
+        // If both of the above calls return Non-NULL, then one key
+        // is present in once subtree and other is present in other,
+        // So this node is the LCA
+        if (l != null && r != null)
+            return root;
 
-        if (root.data == n1)
-            return headOfRoot.data;
 
-        if (data == 0)
-            data = findHead(root.left, n1, root);
-        else
-            return data;
-        if (data == 0)
-            data = findHead(root.right, n1, root);
-        else
-            return data;
+        // Otherwise check if left subtree or right subtree is LCA
+        return (l != null) ? l : r;
 
-        return data;
     }
 }
+
+
+/*
+import java.util.ArrayList;
+import java.util.List;
+
+// A Binary Tree node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+
+public class BT_NoParentPtr_Solution1
+{
+
+    Node root;
+    private List<Integer> path1 = new ArrayList<>();
+    private List<Integer> path2 = new ArrayList<>();
+
+    // Finds the path from root node to given root of the tree.
+    int findLCA(int n1, int n2) {
+        path1.clear();
+        path2.clear();
+        return findLCAInternal(root, n1, n2);
+    }
+
+    private int findLCAInternal(Node root, int n1, int n2) {
+
+        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing");
+            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing");
+            return -1;
+        }
+
+        int i;
+        for (i = 0; i < path1.size() && i < path2.size(); i++) {
+
+        // System.out.println(path1.get(i) + " " + path2.get(i));
+            if (!path1.get(i).equals(path2.get(i)))
+                break;
+        }
+
+        return path1.get(i-1);
+    }
+
+    // Finds the path from root node to given root of the tree, Stores the
+    // path in a vector path[], returns true if path exists otherwise false
+    private boolean findPath(Node root, int n, List<Integer> path)
+    {
+        // base case
+        if (root == null) {
+            return false;
+        }
+
+        // Store this node . The node will be removed if
+        // not in path from root to n.
+        path.add(root.data);
+
+        if (root.data == n) {
+            return true;
+        }
+
+        if (root.left != null && findPath(root.left, n, path)) {
+            return true;
+        }
+
+        if (root.right != null && findPath(root.right, n, path)) {
+            return true;
+        }
+
+        // If not present in subtree rooted with root, remove root from
+        // path[] and return false
+        path.remove(path.size()-1);
+
+        return false;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+
+        System.out.println("LCA(4, 5): " + tree.findLCA(4,5));
+        System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
+        System.out.println("LCA(3, 4): " + tree.findLCA(3,4));
+        System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
+
+    }
+}
+
+ */
