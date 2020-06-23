7eec311a2ae1d8f461923154743b77a3c778a4b8
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2020-06-23 17:30:40 +0530

    Ayush | leetcode array practise

---
+++ src/com/leetcode/array/LastAndFirstElement.java
@@ -0,0 +1,72 @@
+package com.leetcode.array;
+
+public class LastAndFirstElement {
+    public static void main(String[] args) {
+
+        int arr[] = new int[]{1,2,3,3,3,3,3,3,8};
+        int target = 2;
+
+        int[] range = searchRange(arr, target);
+
+        for (int a : range) {
+            System.out.println(a);
+        }
+    }
+
+    private static int[] searchRange(int[] nums, int target) {
+        int arr[] = new int[]{-1, -1};
+
+        if (nums.length < 1) {
+            return arr;
+        }
+
+        arr[0] = searchFirst(nums, target);
+        arr[1] = searchLast(nums, target);
+
+        return arr;
+    }
+
+    private static int searchLast(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, mid;
+
+        if (nums[r] == target)
+            return r;
+
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (nums[mid] == target && nums[mid + 1] > target) {
+                return mid;
+            } else if (nums[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int searchFirst(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, mid;
+
+        if (nums[l] == target)
+            return l;
+
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (nums[mid] == target && nums[mid - 1] < target) {
+                return mid;
+            }
+
+            if (nums[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+
+}
---
+++ src/com/leetcode/array/SearchInRotatedArray.java
@@ -0,0 +1,39 @@
+package com.leetcode.array;
+
+public class SearchInRotatedArray {
+    public static void main(String[] args) {
+
+        int arr[] = new int[]{4,5,6,7,0,1,2};
+        int target = 2;
+        System.out.println(search(arr,target));
+    }
+
+    private static int search(int[] nums, int target) {
+        int l = 0, r = nums.length-1,mid;
+
+        while (l <= r){
+            mid = (l + r)/2;
+            if (nums[mid]==target)
+                return mid;
+
+            if (l == r)
+                break;
+
+            if (nums[l] <= nums[mid]){
+                if (nums[l] <= target && target <= nums[mid]){
+                    r = mid - 1;
+                }else {
+                    l = mid + 1;
+                }
+            }else {
+                if (target >= nums[mid] && target<= nums[r]){
+                    l = mid + 1;
+                }else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
