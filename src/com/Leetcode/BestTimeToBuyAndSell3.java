68a40adc25e687e2cdbaebcfae0d84cf1d75054c
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2020-07-19 10:47:18 +0530

    Ayush | explained BestTimeToBuyAndSell3

--- src/com/Leetcode/BestTimeToBuyAndSell3.java
+++ src/com/Leetcode/BestTimeToBuyAndSell3.java
@@ -16,18 +16,38 @@ public class BestTimeToBuyAndSell3 {
         return maxProfitUtil(prices, 2);
     }
 
+    /*
+    *
+    * Above problem can be thought as, To find the maximum profit in doing at most k transactions, we need to know the maximum profit done
+    * up to any day for an transaction.
+    *
+    * Lets say for one transaction, we got the maximum profit at day 3 and total days are 7, then up to day 7, the max profit for that day will
+    * be what we calculated till day 3.
+    *
+    * Now to get max for two transaction, while looking for maxProfit with 2nd transaction at a day, we must know the max profit done with
+    * 1 transaction before that day, then only we can accumulate the current one with previous maxProfit.
+    *
+    * So, let T[i][j] = It represents the maximum profit done by transaction count 'i', up to jth day.
+    *                           _____________________
+    *                           |
+    *                           |  T[i][j-1], --> meaning we are not considering profit possible with current rate on day j
+    *                    Max{   |  max( price[j] - price[m] + T[i-1][m] ), where m is between 0 to j-1th day
+    *                           |
+    *                           ______________________
+    * */
+
     private static int maxProfitUtil(int[] prices, int k) {
         int T[][] = new int[k + 1][prices.length];
 
         for (int i = 1; i < T.length; i++) {
             int maxDiff = -prices[0];
             for (int j = 1; j < T[0].length; j++) {
-                T[i][j] = Math.max(T[i][j - 1], prices[j] + maxDiff);
-                maxDiff = Math.max(maxDiff, T[i - 1][j] - prices[j]);
+                T[i][j] = Math.max(T[i][j - 1], prices[j] + maxDiff); // Here maxDiff is referring to max from j-1 index
+                maxDiff = Math.max(maxDiff, T[i - 1][j] - prices[j]); //Calculating maxDiff for next day. So index [j] is utilised
             }
         }
 
-        printActualSolution(T,prices);
+        printActualSolution(T, prices);
         return T[k][prices.length - 1];
     }
 
@@ -56,13 +76,13 @@ public class BestTimeToBuyAndSell3 {
             if (i == 0 || j == 0)
                 break;
 
-            if (T[i][j] == T[i][j-1]) {
+            if (T[i][j] == T[i][j - 1]) {
                 j = j - 1;
             } else {
                 stack.addFirst(j);
                 int maxDiff = T[i][j] - prices[j];
                 for (int k = j - 1; k >= 0; k--) {
-                    if (T[i-1][k]-prices[k] == maxDiff){
+                    if (T[i - 1][k] - prices[k] == maxDiff) {
                         i = i - 1;
                         j = k;
                         stack.addFirst(j);
@@ -72,7 +92,7 @@ public class BestTimeToBuyAndSell3 {
             }
         }
 
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             System.out.println("Buy at price : " + prices[stack.pollFirst()]);
             System.out.println("Sell at price : " + prices[stack.pollFirst()]);
         }
