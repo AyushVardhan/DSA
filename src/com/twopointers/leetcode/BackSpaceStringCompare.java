14b6414db30c1a13e57acb47cda4be262ddc8050
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2021-05-22 11:11:14 +0530

    Practise two pointer approach

---
+++ src/com/twopointers/leetcode/BackSpaceStringCompare.java
@@ -0,0 +1,46 @@
+package com.twopointers.leetcode;
+
+public class BackSpaceStringCompare {
+    public static void main(String[] args) {
+        System.out.println(backspaceCompare("ab#c", "ad#c"));
+    }
+
+    private static boolean backspaceCompare(String s, String t) {
+        int n1 = s.length() - 1;
+        int n2 = t.length() - 1;
+
+        while (n1 >= 0 || n2 >= 0) {
+            int idx1 = getNextValidIndex(s, n1);
+            int idx2 = getNextValidIndex(t, n2);
+
+            if (idx1 < 0 && idx2 < 0)
+                return true;
+
+            if (idx1 < 0 || idx2 < 0)
+                return false;
+
+            if (s.charAt(idx1) != t.charAt(idx2))
+                return false;
+
+            n1 = idx1 - 1;
+            n2 = idx2 - 1;
+        }
+
+        return true;
+    }
+
+    private static int getNextValidIndex(String s, int idx) {
+        int backSpaceCount = 0;
+        while (idx >= 0) {
+            if (s.charAt(idx) == '#') {
+                backSpaceCount++;
+            } else if (backSpaceCount > 0) {
+                backSpaceCount--;
+            } else break;
+
+            idx--;
+        }
+
+        return idx;
+    }
+}
---
+++ src/com/twopointers/leetcode/BoatsToSavePeople.java
@@ -0,0 +1,32 @@
+package com.twopointers.leetcode;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+
+//    You are given an array people where people[i] is the weight of the ith person,
+//    and an infinite number of boats where each boat can carry a maximum weight of limit.
+//    Each boat carries at most two people at the same time, provided the sum of the weight
+//    of those people is at most limit.
+//
+//    Return the minimum number of boats to carry every given person.
+
+    public static void main(String[] args) {
+        System.out.println(numRescueBoats(new int[]{4,3,3,5}, 5));
+    }
+
+    private static int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int ans = 0;
+
+        while (i <= j) {
+            ans++;
+            if (people[i] + people[j] <= limit) // matching min weight with max weight to reduce no of boats at end
+                i++;
+            j--;
+        }
+
+        return ans;
+    }
+}
