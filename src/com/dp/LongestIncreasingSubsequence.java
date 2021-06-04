aa5745fd450483dc9072a44f3746a474ca739f55
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2021-06-04 22:27:02 +0530

    practise coding

--- src/com/dp/LongestIncreasingSubsequence.java
+++ src/com/dp/LongestIncreasingSubsequence.java
@@ -5,6 +5,7 @@ public class LongestIncreasingSubsequence {
         System.out.println(lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
     }
 
+    // T(N) -> O(n^2)
     private static int lengthOfLIS(int[] nums) {
         if (nums.length == 0) {
             return 0;
@@ -28,4 +29,26 @@ public class LongestIncreasingSubsequence {
 
         return maxans;
     }
+
+    // Naive approach in mind:
+    /* This wont work. Example - it wont pass this input -> [0,1,0,3,2,3]. Correct ans -> 4. Code gives -> 3. So,
+    we need to use DP and get maxSum from all previous possibilities.
+
+        int res = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int min = 1, max = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[max]) {
+                    min++;
+                    max = j;
+                }
+            }
+
+            res = Math.max(res, min);
+        }
+
+        return res;
+
+     */
 }
